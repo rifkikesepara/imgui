624a598e01ccd23e9eb99364171502a44a44bc74
rifkikesepara <rifkikesepara7@gmail.com>
2022-05-02 11:06:50 +0300

    created

---
+++ premake5.lua
@@ -0,0 +1,55 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+		"backends/imgui_impl_opengl3.cpp",
+		"backends/imgui_impl_opengl3.h",
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_glfw.h"
+	}
+
+	includedirs
+	{
+		"../imgui",
+		"../glfw/include"
+
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
